3221509d5f829239a78fe4c729603603ffff98b7
Onur <onurbelek@outlook.com>
2022-06-01 22:00:11 +0100

    fixing bugs to deploy on heroku

--- uabout/routes/main.py
+++ uabout/routes/main.py
@@ -1,6 +1,5 @@
 from flask import Blueprint, request, render_template
-from ..database.db import db
-from ..models.schema import Listing
+from ..models.schema import User
 
 main_routes = Blueprint("example", __name__)
 
