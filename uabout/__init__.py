8752da38e923d3d52348aeedd0377e47d26a9329
Onur <onurbelek@outlook.com>
2022-06-02 16:01:10 +0100

    test 5

--- uabout/__init__.py
+++ uabout/__init__.py
@@ -18,7 +18,7 @@ if 'postgres' in database_uri:
 
 # ----------------------------- Set up the app ----------------------------------------
 
-app = Flask(__name__, static_folder='my-app/build', static_url_path='')
+app = Flask(__name__, static_folder='client/build', static_url_path='')
 CORS(app)
 
 app.config.update(
