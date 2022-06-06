b627bce2761fb2e7093e2f83c91260a18c72f04f
Robyn <rshortland@hotmail.co.uk>
2022-06-07 00:05:02 +0100

    Added Profile page and icon to Footer

---
+++ uabout/client/src/components/AvatarIcon/index.js
@@ -0,0 +1,80 @@
+import * as React from "react";
+import { styled } from "@mui/material/styles";
+import Badge from "@mui/material/Badge";
+import Avatar from "@mui/material/Avatar";
+import Stack from "@mui/material/Stack";
+
+const StyledBadge = styled(Badge)(({ theme }) => ({
+  "& .MuiBadge-badge": {
+    backgroundColor: "#44b700",
+    color: "#44b700",
+    boxShadow: `0 0 0 2px ${theme.palette.background.paper}`,
+    "&::after": {
+      position: "absolute",
+      top: 0,
+      left: 0,
+      width: "100%",
+      height: "100%",
+      borderRadius: "50%",
+      animation: "ripple 1.2s infinite ease-in-out",
+      border: "1px solid currentColor",
+      content: '""',
+    },
+  },
+  "@keyframes ripple": {
+    "0%": {
+      transform: "scale(.8)",
+      opacity: 1,
+    },
+    "100%": {
+      transform: "scale(2.4)",
+      opacity: 0,
+    },
+  },
+}));
+
+function stringToColor(string: string) {
+  let hash = 0;
+  let i;
+
+  /* eslint-disable no-bitwise */
+  for (i = 0; i < string.length; i += 1) {
+    hash = string.charCodeAt(i) + ((hash << 5) - hash);
+  }
+
+  let color = "#";
+
+  for (i = 0; i < 3; i += 1) {
+    const value = (hash >> (i * 8)) & 0xff;
+    color += `00${value.toString(16)}`.slice(-2);
+  }
+  /* eslint-enable no-bitwise */
+
+  return color;
+}
+
+function stringAvatar(name: string) {
+  return {
+    sx: {
+      bgcolor: stringToColor(name),
+    },
+    children: `${name.split(" ")[0][0]}${name.split(" ")[1][0]}`,
+  };
+}
+
+export default function BackgroundLetterAvatars() {
+  return (
+    <Stack direction="row" spacing={2}>
+      <StyledBadge
+        overlap="circular"
+        anchorOrigin={{ vertical: "bottom", horizontal: "right" }}
+        variant="dot"
+      >
+        <Avatar
+          {...stringAvatar("David Quigley")}
+          sx={{ width: 100, height: 100 }}
+        />
+      </StyledBadge>
+    </Stack>
+  );
+}
--- uabout/client/src/components/Navbar/index.js
+++ uabout/client/src/components/Navbar/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faHome,
@@ -8,17 +9,30 @@ import {
 } from "@fortawesome/free-solid-svg-icons";
 
 const Navbar = () => {
+  const navigate = useNavigate();
   return (
     <nav className="Navbar">
       <ul>
         <li>
-          <FontAwesomeIcon icon={faCalendar} size="3x"></FontAwesomeIcon>
+          <FontAwesomeIcon
+            icon={faCalendar}
+            size="3x"
+            onClick={() => navigate("/myevents")}
+          ></FontAwesomeIcon>
         </li>
         <li>
-          <FontAwesomeIcon icon={faHome} size="3x"></FontAwesomeIcon>
+          <FontAwesomeIcon
+            icon={faHome}
+            size="3x"
+            onClick={() => navigate("/home")}
+          ></FontAwesomeIcon>
         </li>
         <li>
-          <FontAwesomeIcon icon={faSearch} size="3x"></FontAwesomeIcon>
+          <FontAwesomeIcon
+            icon={faSearch}
+            size="3x"
+            onClick={() => navigate("/search")}
+          ></FontAwesomeIcon>
         </li>
         <li>
           <FontAwesomeIcon icon={faComments} size="3x"></FontAwesomeIcon>
