03208eb69c626834bb31fcd02a3ee4921179a40d
Andrew Kennedy <andrew.kennedy22@hotmail.com>
2022-06-07 14:30:52 +0100

    FriendBox Component created
    Card display will render for each search result on the 'Search Page'
    
    Initially hardcoded, but now can be called as a Component. Example also added to the FriendPage index.js for display example. Username is hardcoded so will need to work on DB logic

--- uabout/client/src/components/index.js
+++ uabout/client/src/components/index.js
@@ -4,3 +4,4 @@ export { default as ReactCalendar } from "./ReactCalendar";
 export { default as EventForm } from "./EventForm";
 export { default as EventCard } from "./EventCard";
 export { default as BackgroundLetterAvatars } from "./AvatarIcon";
+export { default as FriendBox } from "./FriendBox";
