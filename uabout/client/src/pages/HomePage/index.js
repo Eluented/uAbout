8d5d507b828424f142a74b10bb38dcf118f6af9f
Onur <onurbelek@outlook.com>
2022-06-07 23:38:55 +0100

    adding function status returns for home page - stops the map error

--- uabout/client/src/pages/HomePage/index.js
+++ uabout/client/src/pages/HomePage/index.js
@@ -19,43 +19,52 @@ const HomePage = () => {
 
   const getPosts = useSelector(postsResult)
 
+  if (searchPostStatus === "loading"){
+    return <h1>Loading ...</h1>
+  }
 
-  return (
-    <div className="homepage-container">
-      <Navbar />
-      <div className="homepage-section">
-        <div className="eventform-parent-container">
-          <button
-            className="openModalBtn"
-            onClick={() => {
-              setModalOpen(true);
-            }}
-          >
-            Create an event David!
-          </button>
-          {modalOpen && <EventForm setOpenModal={setModalOpen} />}
+  if (searchPostStatus === "failed") {
+    <h1>Failed to display posts! Please try again</h1>
+  }
+
+  if (searchPostStatus === "succeeded"){
+    return (
+      <div className="homepage-container">
+        <Navbar />
+        <div className="homepage-section">
+          <div className="eventform-parent-container">
+            <button
+              className="openModalBtn"
+              onClick={() => {
+                setModalOpen(true);
+              }}
+            >
+              Create an event David!
+            </button>
+            {modalOpen && <EventForm setOpenModal={setModalOpen} />}
+          </div>
+          <div className="eventrender-container">
+  
+            {searchPostStatus === "succeeded" &&
+              getPosts.map(({ post_body, post_title, post_id, event_start, event_end }, idx) => (
+                <EventCard className="event-cards"
+                  post_body={post_body}
+                  post_title={post_title}
+                  post_id={post_id}
+                  event_start={event_start}
+                  event_end={event_end}
+                  key={idx} />
+              ))}
+  
+  
+          </div>
+          : <h1>...</h1>
         </div>
-        <div className="eventrender-container">
-
-          {searchPostStatus === "succeeded" &&
-            getPosts.map(({ post_body, post_title, post_id, event_start, event_end }, idx) => (
-              <EventCard className="event-cards"
-                post_body={post_body}
-                post_title={post_title}
-                post_id={post_id}
-                event_start={event_start}
-                event_end={event_end}
-                key={idx} />
-            ))}
-
-          {searchPostStatus === "failed" &&
-            <h1>There are no posts to display</h1>}
-        </div>
-        : <h1>...</h1>
+        <Footer />
       </div>
-      <Footer />
-    </div>
-  );
+    );
+  }
+
 };
 
 export default HomePage;
