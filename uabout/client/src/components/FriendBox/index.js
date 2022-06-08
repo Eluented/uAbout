26805959c3393bd7e05ce23c8b48125bf9c715ca
Robyn <rshortland@hotmail.co.uk>
2022-06-08 11:50:23 +0100

    edited friend box and search result

--- uabout/client/src/components/FriendBox/index.js
+++ uabout/client/src/components/FriendBox/index.js
@@ -1,16 +1,25 @@
 import "./index.css";
 import BackgroundLetterAvatars from "../AvatarIcon";
+import { Button } from "@mui/material";
 
-const FriendBox = () => {
+const FriendBox = ({ username, first_name, last_name, idx }) => {
+  function sendFriendRequest() {
+    console.log();
+  }
   return (
     <>
-      <div className="friend_list">
+      <div key={idx} className="friend_list">
         <div className="friend_card">
           <div class="Avatar">
             <BackgroundLetterAvatars />
           </div>
-          <div class="NameBox">Andrew Kennedy</div>
-          <div class="UsernameBox">agk205</div>
+          <div class="NameBox">
+            {first_name} {last_name}
+          </div>
+          <div class="UsernameBox">{username}</div>
+          <Button variant="outlined" onClick={sendFriendRequest()}>
+            Add Friend
+          </Button>
         </div>
       </div>
     </>
