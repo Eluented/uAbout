20700b8dc83b43fed7e2f04ba08fbd3a62d4ae05
Robyn <rshortland@hotmail.co.uk>
2022-06-09 09:15:15 +0100

    Added my events to calendar page

--- uabout/client/src/pages/CalendarPage/index.js
+++ uabout/client/src/pages/CalendarPage/index.js
@@ -1,19 +1,85 @@
 import React from "react";
-import { Navbar, Footer, ReactCalendar } from "../../components";
+import { Footer, Navbar, EventCard, ReactCalendar } from "../../components";
+import { postsResult } from "../../reducers/mainSlice";
+import { useSelector } from "react-redux";
 
 const CalendarPage = () => {
-  return (
-    <div className="myevents-container">
-      <Navbar />
-      <div className="homepage-section">
-        <div className="calendar-parent-container">
-          <ReactCalendar />
-        </div>
-        <div className="events-parent-container"></div>
-      </div>
-      <Footer />
-    </div>
-  );
+  const searchPostStatus = useSelector((state) => state.main.status);
+
+  const getPosts = useSelector(postsResult);
+
+  if (searchPostStatus === "loading") {
+    return <h1>Loading ...</h1>;
+  }
+
+  if (searchPostStatus === "failed") {
+    return <h1>Failed to display posts! Please try again</h1>;
+  }
+
+  if (searchPostStatus === "succeeded") {
+    return (
+      <>
+        {" "}
+        {!getPosts.length ? (
+          <div className="myevents-container">
+            <Navbar />
+            <div className="homepage-section">
+              <div className="calendar-parent-container">
+                <ReactCalendar />
+                <div>
+                  <h1>My Events</h1>
+                  <div className="eventrender-container">
+                    <h1>There are no current events... Make one now sir</h1>
+                  </div>
+                </div>
+              </div>
+              <div className="events-parent-container"></div>
+            </div>
+            <Footer />
+          </div>
+        ) : (
+          <div className="myevents-container">
+            <Navbar />
+            <div className="homepage-section">
+              <div className="calendar-parent-container">
+                <ReactCalendar />
+                <div>
+                  <h1>My Events</h1>
+                  <div className="eventrender-container">
+                    {searchPostStatus === "succeeded" &&
+                      getPosts.map(
+                        (
+                          {
+                            post_body,
+                            post_title,
+                            post_id,
+                            event_start,
+                            event_end,
+                          },
+                          idx
+                        ) => (
+                          <EventCard
+                            className="event-cards"
+                            post_body={post_body}
+                            post_title={post_title}
+                            post_id={post_id}
+                            event_start={event_start}
+                            event_end={event_end}
+                            key={idx}
+                          />
+                        )
+                      )}
+                  </div>
+                </div>
+              </div>
+              <div className="events-parent-container"></div>
+            </div>
+            <Footer />
+          </div>
+        )}
+      </>
+    );
+  }
 };
 
 export default CalendarPage;
--- uabout/client/src/pages/NotFoundPage/index.js
+++ uabout/client/src/pages/NotFoundPage/index.js
@@ -1,6 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
+import { EventForm, EventCard } from "../../components";
 
 const NotFoundPage = () => {
-  return <div>NotFoundPage</div>;
+  const [modalOpen, setModalOpen] = useState(false);
+  return (
+    <>
+      <div>NotFoundPage</div>
+      <div className="homepage-section">
+        <div className="eventform-parent-container">
+          <button
+            className="openModalBtn"
+            onClick={() => {
+              setModalOpen(true);
+            }}
+          >
+            Create an event
+          </button>
+          {modalOpen && <EventForm setOpenModal={setModalOpen} />}
+        </div>
+        <div className="eventrender-container">
+          <EventCard />
+        </div>
+        : <h1>...</h1>
+      </div>
+    </>
+  );
 };
 export default NotFoundPage;
