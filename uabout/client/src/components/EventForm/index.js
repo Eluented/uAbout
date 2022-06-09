f84f7a9a2180e825b84f18cdb2f11aea4e731a1a
Onur <onurbelek@outlook.com>
2022-06-09 10:57:42 +0100

    test

--- uabout/client/src/components/EventForm/index.js
+++ uabout/client/src/components/EventForm/index.js
@@ -103,6 +103,67 @@ function EventForm({ setOpenModal }) {
 
   console.log(event);
 
+  if (allFriends.length === 0){
+    return (
+      <>
+      <div className="form-container">
+        <div>
+          <button className="modalCloseBtn" onClick={() => setOpenModal(false)}>
+            x
+          </button>
+        </div>
+        <form
+          id="event-form"
+          noValidate
+          autoComplete="off"
+          onSubmit={handleSubmit}
+        >
+          <TextField
+            className="input-fields"
+            id="outlined-event-name"
+            label="Event Name"
+            value={formData.post_title}
+            onChange={(e) => handleChange(e)}
+            name="post_title"
+          />{" "}
+          <br />
+          <div className="date-container">
+            <label>
+              Start Date:
+              <DatePicker
+                className="date-input-field"
+                onChange={(date) => setStartDate(date)}
+                selected={startDate}
+                name="start_date"
+              />
+            </label>
+            <label>
+              End Date:
+              <DatePicker
+                className="date-input-field"
+                onChange={(date) => setEndDate(date)}
+                selected={endDate}
+                name="end_date"
+              />
+            </label>
+          </div>
+          <TextField
+            className="input-fields"
+            id="outlined-body-input"
+            label="Event Details"
+            value={formData.post_body}
+            onChange={(e) => handleChange(e)}
+            name="post_body"
+          />
+          <br />
+          <Button variant="contained" id="post-btn" type="submit">
+            Post
+          </Button>
+        </form>
+      </div>
+    </> 
+    )
+  } 
   return (
     <>
       <div className="form-container">
