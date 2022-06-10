af19ce0a8808b6aae81f2f87c3f980fa31a6187a
Andrew Kennedy <andrew.kennedy22@hotmail.com>
2022-06-10 11:13:01 +0100

    Friend Request Box styling fix

--- uabout/client/src/components/FriendRequest/index.js
+++ uabout/client/src/components/FriendRequest/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import httpClient from "../../httpClient";
+import './index.css'
 
 function FriendRequest({ first_name, last_name, user_id, username, status, idx }) {
 
@@ -38,12 +39,29 @@ function FriendRequest({ first_name, last_name, user_id, username, status, idx }
   }
   if (status === "Sent Friend Requests"){
     return (
-      <span key={idx} >
-      <p>{first_name} {last_name} {username}</p>
-      <p>{status}</p>
-      <button onClick={acceptFriendRequest}>Accept</button>
-      <button>Decline</button>
-    </span>
+      <>
+      <div key={idx} className="request_list">
+        <div className="request_card">
+          <div class="Avatar">
+            <BackgroundLetterAvatars />
+          </div>
+          <div class="RequestName">
+            {first_name} {last_name}
+          </div>
+          <div class="UsernameRequest">{username}</div>
+          <div class="AcceptButton">
+            <Button variant="outlined" onClick={acceptFriendRequest}>
+              Accept
+            </Button>
+          </div>
+          <div class="DeclineButton">
+            <Button variant="outlined">
+              Decline
+            </Button>
+          </div>
+        </div>
+      </div>
+    </>
     )
   }
 }
