ae2410846fd12480038b289a78601394560071a0
Onur <onurbelek@outlook.com>
2022-06-16 23:42:58 +0100

    rebuild

--- uabout/client/src/pages/CalendarPage/index.js
+++ uabout/client/src/pages/CalendarPage/index.js
@@ -2,11 +2,13 @@ import React from "react";
 import { Footer, Navbar, ReactCalendar } from "../../components";
 
 const CalendarPage = () => {
+  return (
   <>
     <Navbar />
     <ReactCalendar />
     <Footer />
   </>
+  )
 };
 
 export default CalendarPage;
