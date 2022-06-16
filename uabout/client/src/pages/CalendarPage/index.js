5d62d0371ed763bf18724b06512ecf820b0a0081
Onur <onurbelek@outlook.com>
2022-06-16 21:42:14 +0100

    rebuild

--- uabout/client/src/pages/CalendarPage/index.js
+++ uabout/client/src/pages/CalendarPage/index.js
@@ -1,52 +1,12 @@
 import React from "react";
-import { Footer, Navbar, EventCard, ReactCalendar } from "../../components";
-import { postsResult } from "../../reducers/mainSlice";
-import { useSelector } from "react-redux";
+import { Footer, Navbar, ReactCalendar } from "../../components";
 
 const CalendarPage = () => {
-  const searchPostStatus = useSelector((state) => state.main.status);
-
-  const getPosts = useSelector(postsResult);
-
-  if (searchPostStatus === "loading") {
-    return <h1>Loading ...</h1>;
-  }
-
-  if (searchPostStatus === "failed") {
-    return <h1>Failed to display posts! Please try again</h1>;
-  }
-
-  if (searchPostStatus === "succeeded") {
-    return (
-      <>
-        {" "}
-        {!getPosts.length ? (
-          <div className="myevents-container">
-            <Navbar />
-            <div className="homepage-section">
-              <div className="calendar-parent-container">
-                <ReactCalendar />
-                <div id="my-event-container">
-                  <h1>My Events</h1>
-                  <div className="my-eventrender-container">
-                    <h1>You have no current events!</h1>
-                  </div>
-                </div>
-              </div>
-              <div className="events-parent-container"></div>
-            </div>
-            <Footer />
-          </div>
-        ) : (
-          <div className="myevents-container">
-            <Navbar />
-            <ReactCalendar />
-            <Footer />
-          </div>
-        )}
-      </>
-    );
-  }
+  <>
+    <Navbar />
+    <ReactCalendar />
+    <Footer />
+  </>
 };
 
 export default CalendarPage;
