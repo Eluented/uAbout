93ef60bf90e898b94b6d8a769b11694aee490aaa
Onur <onurbelek@outlook.com>
2022-06-16 23:10:49 +0100

    rebuild

--- uabout/client/src/components/EventCard/index.js
+++ uabout/client/src/components/EventCard/index.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { styled } from "@mui/material/styles";
 import Card from "@mui/material/Card";
 import CardHeader from "@mui/material/CardHeader";
-// import CardMedia from "@mui/material/CardMedia";
+
 import CardContent from "@mui/material/CardContent";
 import CardActions from "@mui/material/CardActions";
 import Collapse from "@mui/material/Collapse";
